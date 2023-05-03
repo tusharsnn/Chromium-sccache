3cd559809ca847bf2f9626e3876b138a636688ce
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-03 21:03:14 +0530

    remove build tool setup from other parts of the script

--- scripts/fetch-chromium.ps1
+++ scripts/fetch-chromium.ps1
@@ -4,10 +4,6 @@ git config --global core.autocrlf false
 git config --global core.filemode false
 git config --global branch.autosetuprebase always
 
-# setup depot_tools
-.\scripts\setup-depot-tools.ps1
-$env:PATH="$env:DEPOT_TOOLS_PATH;$env:PATH"
-
 # fetch chromium checkout
 mkdir $env:CHROMIUM_PATH && cd $env:CHROMIUM_PATH
 fetch --no-history chromium
@@ -17,7 +13,7 @@ cd $env:CHROMIUM_PATH\src
 gn gen out\Default `
     --args="is_component_build = true" `
     --args="enable_nacl = false" `
-    --args="target_cpu = x86" `
+    --args="target_cpu = x64" `
     --args="blink_symbol_level = 0" `
     --args="v8_symbol_level = 0" `
     --args="symbol_level = 0" `
--- scripts/setup-sccache.ps1
+++ scripts/setup-sccache.ps1
@@ -1,6 +1,6 @@
 Invoke-Webrequest -Uri $env:SCCACHE_URI -OutFile sccache.tar.gz
 tar -xvzf sccache.tar.gz -C "$env:SCCACHE_TOOL_PATH\.."
-ls "$env:SCCACHE_TOOL_PATH/$env:SCCACHE_FILENAME"
+ls "$env:SCCACHE_TOOL_PATH"
 rm -force sccache.tar.gz
 echo "$env:SCCACHE_TOOL_PATH" >> "$env:GITHUB_PATH"
 Write-Host "sccache installed"
