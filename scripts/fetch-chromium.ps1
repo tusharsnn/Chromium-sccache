cb3d26545871e84681b1d8165134fe514e323821
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-03 19:57:24 +0530

    add depot-tools to $PATH

--- scripts/fetch-chromium.ps1
+++ scripts/fetch-chromium.ps1
@@ -6,14 +6,15 @@ git config --global branch.autosetuprebase always
 
 # setup depot_tools
 .\scripts\setup-depot-tools.ps1
+$env:PATH="$env:DEPOT_TOOLS_PATH;$env:PATH"
 
 # fetch chromium checkout
 mkdir $env:CHROMIUM_PATH && cd $env:CHROMIUM_PATH
-& "$env:DEPOT_TOOLS_PATH\fetch" --no-history chromium
+fetch --no-history chromium
 ls $env:CHROMIUM_PATH\src
 
 cd $env:CHROMIUM_PATH\src
-& "$env:DEPOT_TOOLS_PATH\gn" gen out\Default `
+gn gen out\Default `
     --args="is_component_build = true" `
     --args="enable_nacl = false" `
     --args="target_cpu = x86" `
