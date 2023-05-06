874fc76e560591da0299d4631f093dd15bf52082
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-06 15:02:21 +0530

    fix gn --args format

--- scripts/fetch-chromium.ps1
+++ scripts/fetch-chromium.ps1
@@ -12,14 +12,7 @@ ls $env:CHROMIUM_PATH\src
 
 cd $env:CHROMIUM_PATH\src
 gn gen out\Default `
-    --args="is_component_build = true" `
-    --args="enable_nacl = false" `
-    --args="target_cpu = x64" `
-    --args="blink_symbol_level = 0" `
-    --args="v8_symbol_level = 0" `
-    --args="symbol_level = 0" `
-    --args="cc_wrapper = sccache" `
-    --args="chrome_pgo_phase = 0"
+    --args="is_component_build=true enable_nacl=false target_cpu=x64 blink_symbol_level=0 v8_symbol_level=0 symbol_level=0 cc_wrapper=sccache chrome_pgo_phase=0"
 
 echo "gn build args:"
-gn args out\Default --list
+gn args out\Default --list=cc_wrapper
