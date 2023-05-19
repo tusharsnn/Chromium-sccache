4b23c43dec6ac3f5b3c246472f2f937e2a7abd0a
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-19 19:31:52 +0530

    test -Verb runAs

--- scripts/setup-msvc.ps1
+++ scripts/setup-msvc.ps1
@@ -1,4 +1,3 @@
 $vs_enterprise_path = (& "$env:MSVC_INSTALLER\vswhere.exe" -property installationPath)
-$vs_enterprise_path
-Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'uninstall --installPath $vs_enterprise_path --quiet --norestart' -Wait *>&1
-Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'install --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.17.Release --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart' -Wait *>&1
+Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'uninstall --installPath $vs_enterprise_path --quiet --norestart' -Wait -Verb runAs
+Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'install --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.17.Release --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart' -Wait -Verb runAs
