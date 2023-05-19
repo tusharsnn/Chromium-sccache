f7535cde33f1d9cc9a77ddde7c6da89189ae2eb7
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-19 21:35:15 +0530

    fix required envs

--- scripts/setup-msvc.ps1
+++ scripts/setup-msvc.ps1
@@ -1,7 +1,12 @@
+if (-not $env:MSVC_INSTALLER) {
+    echo "not found MSVC_INSTALLER"
+    $env:MSVC_INSTALLER='C:\Program Files (x86)\Microsoft Visual Studio\Installer'
+}
 $vs_enterprise_path = (& "$env:MSVC_INSTALLER\vswhere.exe" -property installationPath)
-Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'uninstall --installPath $vs_enterprise_path --quiet --norestart' -Wait -Verb runAs -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+$vs_enterprise_path
+Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'uninstall --installPath $vs_enterprise_path --quiet --norestart' -Wait -RSO stdout.txt -RSE stderr.txt
 cat stdout.txt
 cat stderr.txt
-Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'install --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.17.Release --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart' -Wait -Verb runAs -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'install --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.17.Release --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart' -Wait -RSO stdout.txt -RSE stderr.txt
 cat stdout.txt
 cat stderr.txt
