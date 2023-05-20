0912b238965857c0728036439f9038493b1d4e0c
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-20 12:25:49 +0530

    try winget

--- scripts/setup-msvc.ps1
+++ scripts/setup-msvc.ps1
@@ -4,9 +4,9 @@ if (-not $env:MSVC_INSTALLER) {
 }
 $vs_enterprise_path = (& "$env:MSVC_INSTALLER\vswhere.exe" -property installationPath)
 $vs_enterprise_path
-Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'uninstall --installPath $vs_enterprise_path --quiet --norestart' -Wait -RSO stdout.txt -RSE stderr.txt
+Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList "uninstall --installPath $vs_enterprise_path --quiet --norestart" -Wait -RSO stdout.txt -RSE stderr.txt
 cat stdout.txt
 cat stderr.txt
-Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList 'install --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.17.Release --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart' -Wait -RSO stdout.txt -RSE stderr.txt
+Start-Process "$env:MSVC_INSTALLER\setup.exe" -ArgumentList "install --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.17.Release --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart" -Wait -RSO stdout.txt -RSE stderr.txt
 cat stdout.txt
 cat stderr.txt
