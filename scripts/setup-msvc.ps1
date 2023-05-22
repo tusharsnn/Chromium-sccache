10501fcc9ecc376aed12667a7951ce9614a7ca22
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-22 12:17:59 +0530

    use gsudo for shell elevation

--- scripts/setup-msvc.ps1
+++ scripts/setup-msvc.ps1
@@ -1,12 +1,25 @@
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    echo "Please run as administrator"
+    exit 1
+}
 if (-not $env:MSVC_INSTALLER) {
     echo "not found MSVC_INSTALLER"
     $env:MSVC_INSTALLER='C:\Program Files (x86)\Microsoft Visual Studio\Installer'
 }
-$vs_enterprise_path = (& "$env:MSVC_INSTALLER\vswhere.exe" -property installationPath)
-$vs_enterprise_path
-$vs_enterprise_url = 'https://aka.ms/vs/17/release/vs_enterprise.exe'
-$vs_community_url = 'https://aka.ms/vs/17/release/vs_community.exe'
-Invoke-WebRequest -Uri $vs_enterprise_url -OutFile vs_enterprise.exe
-Start-Process "./vs_enterprise.exe" -ArgumentList "uninstall --quiet --norestart" -Wait
-Invoke-WebRequest -Uri $vs_community_url -OutFile vs_community.exe
-Start-Process "./vs_community.exe" -ArgumentList 'install --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended --quiet --norestart' -Wait
+$vs_enterprise_path = (& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" `
+    -requires Microsoft.VisualStudio.Product.Enterprise `
+    -property installationPath `
+) 
+if ($vs_enterprise_path) {
+    echo "uninstalling: $vs_enterpise_path"
+    cmd /c "$env:MSVC_INSTALLER\setup.exe" uninstall `
+        --installPath $vs_enterprise_path --quiet --norestart
+}
+cmd /c "$env:MSVC_INSTALLER\setup.exe" install `
+    --productId Microsoft.VisualStudio.Product.Community `
+    --channelId VisualStudio.17.Release `
+    --add Microsoft.VisualStudio.Component.Windows11SDK.22621 `
+    --add Microsoft.VisualStudio.Workload.NativeDesktop `
+    --add Microsoft.VisualStudio.Component.VC.ATLMFC `
+    --includeRecommended `
+    --quiet --norestart
