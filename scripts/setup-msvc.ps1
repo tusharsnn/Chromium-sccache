a2356efd91bf76796725daf62c7310cb5bd87939
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-22 18:57:04 +0530

    use productId and channelId instead of installPath for uninstall

--- scripts/setup-msvc.ps1
+++ scripts/setup-msvc.ps1
@@ -6,14 +6,13 @@ if (-not $env:MSVC_INSTALLER) {
     echo "not found MSVC_INSTALLER"
     $env:MSVC_INSTALLER='C:\Program Files (x86)\Microsoft Visual Studio\Installer'
 }
-$vs_enterprise_path = (& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" `
-    -requires Microsoft.VisualStudio.Product.Enterprise `
-    -property installationPath `
-) 
-if ($vs_enterprise_path) {
-    echo "uninstalling: $vs_enterprise_path"
+$vs_product_id = (& "$env:MSVC_INSTALLER\vswhere.exe" -property productId) 
+$vs_channel_id = (& "$env:MSVC_INSTALLER\vswhere.exe" -property channelId) 
+if ($vs_product_id -eq 'Microsoft.VisualStudio.Product.Enterprise') {
+    echo "uninstalling: $vs_product_id ($vs_channel_id)"
     cmd /c "$env:MSVC_INSTALLER\setup.exe" uninstall `
-        --installPath "'$vs_enterprise_path'" `
+        --productId $vs_product_id `
+        --channelId $vs_channel_id `
         --quiet --norestart
 }
 cmd /c "$env:MSVC_INSTALLER\setup.exe" install `
